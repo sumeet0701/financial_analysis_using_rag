30b1ca69167dab4fea1b608140ea4e73a06a0165
sumeet <maheshwarisumeet128@gmail.com>
2024-09-29 23:45:11 +0530

    pipeline template created.

---
+++ Rag/pipeline/pipeline.py
@@ -0,0 +1,19 @@
+from Rag.componets.web_scraping import WebScraper
+from Rag.componets.chunking import Chunking
+from Rag.logger import logging
+from Rag.exception import RagException
+from Rag.constant import *
+
+class Pipeline:
+
+    def __init__(self):...
+
+    def webscraper(self):...
+
+    def chunking(self):...
+
+    def embedding(self):...
+
+    def vector_database_dumping(self):...
+
+    def RAG(self):...
