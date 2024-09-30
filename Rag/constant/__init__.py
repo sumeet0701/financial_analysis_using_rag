85156d34e71d0e56ecb9ddeafc2ea2f10861aad5
sumeet <maheshwarisumeet128@gmail.com>
2024-09-30 23:42:59 +0530

    Pipeline updated completely.

--- Rag/constant/__init__.py
+++ Rag/constant/__init__.py
@@ -1,3 +1,5 @@
 HUGGING_FACE_EMBEDDING_MODEL = "nomic-ai/nomic-embed-text-v1.5"
 WEBPAGE_URL_TEMPLATE = "https://ticker.finology.in/company/{}"
 GOOGLE_EMBEEDING_MODEL = "models/text-embedding-004"
+CHUNK_SIZE = 512
+CHUNK_OVERLAP = 100
--- Rag/pipeline/pipeline.py
+++ Rag/pipeline/pipeline.py
@@ -1,19 +1,44 @@
 from Rag.componets.web_scraping import WebScraper
 from Rag.componets.chunking import Chunking
+from Rag.componets.vector_db import Embedding
+from Rag.componets.vector_db import VectorEmbeddingStoring
 from Rag.logger import logging
 from Rag.exception import RagException
 from Rag.constant import *
 
 class Pipeline:
 
-    def __init__(self):...
+    def __init__(self):
+        self.webpage_url = WEBPAGE_URL_TEMPLATE
+        self.chunk_size = CHUNK_SIZE
+        self.chunk_overlap = CHUNK_OVERLAP
+    def webscraper(self):
+        web_scrapper = WebScraper(path= self.webpage_url)
+        data = web_scrapper.scrape()
+        return data
+    
+    def chunking(self, data):
+        chunking_instance = Chunking()
+        nodes = chunking_instance.chunking_using_recursive(
+            documents= data,
+            chunk_overlap= self.chunk_overlap,
+            chunk_size= self.chunk_size
+        )
+        return nodes
 
-    def webscraper(self):...
-
-    def chunking(self):...
-
-    def embedding(self):...
-
-    def vector_database_dumping(self):...
-
+    
+    def vector_database_dumping(self, nodes):
+        vector_embedding = VectorEmbeddingStoring(collection= "web_data")
+        loading_data_disk = vector_embedding.saving_vector_database(nodes = nodes)
+        loading_data_from_database = vector_embedding.loading_vector_database()
+        return loading_data_from_database
+    
     def RAG(self):...
+
+    def main():
+        data = Pipeline.webscraper()
+        nodes = Pipeline.chunking(data= data)
+        vector = Pipeline.vector_database_dumping(nodes= nodes)
+    
+    
+
