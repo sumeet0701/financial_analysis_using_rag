e31bd7b871e0c287bd3f13e8ff6f7e954e51f37e
sumeet <maheshwarisumeet128@gmail.com>
2024-10-01 23:58:25 +0530

    try_except block updated

--- Rag/componets/vector_db.py
+++ Rag/componets/vector_db.py
@@ -28,7 +28,7 @@ class Embedding:
     def embedding_document(self):
         try:
         # get API key and create embeddings
-            logging.info("embedding of documents is started...")
+            logging.info("embedding of Model loading is started...")
 
             model_name = self.model_name
 
@@ -37,7 +37,7 @@ class Embedding:
                 api_key=self.api_key, 
             )
 
-            logging.info("embedding of documents is completed successfully..")
+            logging.info("embedding model is loadde. completed successfully..")
             return embed_model
         except Exception as e:
             logging.error("Error Embedding document:", exc_info=True)  # Log detailed error information
@@ -52,27 +52,39 @@ class VectorEmbeddingStoring:
         self.embedder = Embedding(api_key, model_name)
     
     def saving_vector_database(self, nodes):
-        embed_model = self.embedder.embedding_document()
-        vector_store = ChromaVectorStore(chroma_collection = self.collection)
-        storage_context = StorageContext.from_defaults(vector_store =  vector_store)
-        index = VectorStoreIndex( 
-            nodes,
-            storage_context = storage_context,
-            embed_model = embed_model
-            
-        )
-        return index
+        try:
+            logging.info("embedding of document is started...")
+            embed_model = self.embedder.embedding_document()
+            vector_store = ChromaVectorStore(chroma_collection = self.collection)
+            storage_context = StorageContext.from_defaults(vector_store =  vector_store)
+            index = VectorStoreIndex( 
+                nodes,
+                storage_context = storage_context,
+                embed_model = embed_model
+                
+            )
+            logging.info("Data is stored sucessfully in vector database....")
+            return index
+        except Exception as e:
+            logging.error("Error Embedding document:", exc_info=True)  # Log detailed error information
+            raise RagException(e) from e
     
     def loading_vector_database(self):
-        db = self.client
-        embed_model = self.embedder.embedding_document()
-        collection = db.get_or_create_collection(collection)
-        vector_store = ChromaVectorStore(chroma_collection=collection)
-        index = VectorStoreIndex.from_vector_store(
-            vector_store,
-            embed_model=embed_model
-        )
-        return index
+        try:
+            logging.info("Loading vector embedding from database...")
+            db = self.client
+            embed_model = self.embedder.embedding_document()
+            collection = db.get_or_create_collection(collection)
+            vector_store = ChromaVectorStore(chroma_collection=collection)
+            index = VectorStoreIndex.from_vector_store(
+                vector_store,
+                embed_model=embed_model
+            )
+            logging.info("vector embedding of the document retrived sucessfully")
+            return index
+        except Exception as e:
+            logging.error("Error Embedding document:", exc_info=True)  # Log detailed error information
+            raise RagException(e) from e
     
 
 
